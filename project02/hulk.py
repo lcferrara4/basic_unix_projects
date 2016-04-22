65ea8c1c0d62f990be8fbbb2d963f99b9e4cd231
Teddy Brombach <Teddy@teddys-mbp.dhcp.nd.edu>
2016-04-22 00:06:39 -0400

    Have not tested get ops

--- project02/hulk.py
+++ project02/hulk.py
@@ -5,6 +5,7 @@ import string
 import random
 import hashlib
 import itertools
+import getopt
 
 #constatss
 
@@ -12,6 +13,7 @@ ALPHABET = string.ascii_lowercase + string.digits
 LENGTH = int(sys.argv[1])
 #ATTEMPTS = int(sys.argv[2]) cause doing it systematically
 HASHES = sys.argv[3]
+PREFIX = ''
 
 
 #Utility functions
@@ -29,26 +31,49 @@ Options:
 	sys.exit(exit_code)
 
 def md5sum(s):
-	return hashlin.md5(s).hexdigest()
+	return hashlib.md5(s).hexdigest()
 
-# Parsing the command line
 
 
 #Main Exacution
 
 if __name__ == '__main__':
+	# Parsing the command line
+	count = 1
+	try:
+		options, arguments = getopt.getopt(sys.argv[1:], "a:l:s:p:")
+	except getopt.GetoptError as e:
+		usage(1)
+
+	for option, value in options:
+		if option == '-a':
+			ALPHABET = str(value)
+			count+=2
+		elif option == '-l':
+			LENGTH = int(value)
+			count+=2
+		elif option == '-p':
+			PREFIX = str(value)
+			count+=2
+		elif option == '-s':
+			HASHES = str(value)
+			cout+=1
+
+
 	hashes = set([l.strip() for l in open(HASHES)])
 	found = set()
 
-	for canidate in itertools.prodcut(ALPHABET, repeat=LENGTH):
+	for canidate in itertools.product(ALPHABET, repeat=LENGTH):
 		canidate = ''.join(canidate)
 		#canidate = ''.join([random.choice(ALPHABET) for _ in range(LENGHT)])
 
-		print canidate
+		print canidate # all the canidates that the hash could be
 
-		checksum = md5sum(candiate)
+		checksum = md5sum(canidate)
 		if checksum in hashes:
-			found.add(candidate)
+			#found.add(candidate) 
+			found.add(canidate)
+			print "Check sum" ,checksum
 
 	for word in sorted(found):
-		print word
+		print "Word" ,word

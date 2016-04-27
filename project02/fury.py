66c4764f9db17cff9ee4f3f439ed5f756492d3b2
Emily Claire Obaditch <eobaditc@student02.cse.nd.edu>
2016-04-27 15:40:42 -0400

    updated fury

--- project02/fury.py
+++ project02/fury.py
@@ -9,11 +9,11 @@ import work_queue
 
 #Main Execution
 
-JOURNAL = {}
+JOURNAL = json.load(open("journal.json"))
 ALPHABET = string.ascii_lowercase + string.digits
 
 if __name__ == '__main__':
-    queue = work_queue.WorkQueue(9200, name ='hulk-eobaditc',catalog=True)
+    queue = work_queue.WorkQueue(9150, name ='hulk-eobaditc',catalog=True)
     queue.specify_log('fury.log')
 
 
@@ -21,20 +21,24 @@ if __name__ == '__main__':
         command = './hulk.py -l {}'.format(i)
         if command in JOURNAL:
             print >>sys.stderr,'Already did',command
-        task = work_queue.Task(command)
-        for source in ('hulk.py','hashes.txt'):
-            task.specify_file(source, source, work_queue.WORK_QUEUE_INPUT)
-        queue.submit(task)
-
-    for i in range(1,2):
-        for prefix in itertools.product(ALPHABET,repeat=i):
-            command = './hulk.py -l 6 -p {}'.format(prefix)
-            if command in JOURNAL:
-                print >>sys.stderr,'Already did',command
+        if not command in JOURNAL:
             task = work_queue.Task(command)
             for source in ('hulk.py','hashes.txt'):
                 task.specify_file(source, source, work_queue.WORK_QUEUE_INPUT)
             queue.submit(task)
+
+    for i in range(1,4):
+        for prefix in itertools.product(ALPHABET,repeat=i):
+            prefix=''.join(prefix)
+            command = './hulk.py -l 5 -p {}'.format(prefix)
+            if command in JOURNAL:
+                print >>sys.stderr,'Already did',command
+            else:
+                task = work_queue.Task(command)
+                for source in ('hulk.py','hashes.txt'):
+                    task.specify_file(source, source, work_queue.WORK_QUEUE_INPUT)
+                queue.submit(task)
+
 
     while not queue.empty():
         task = queue.wait()
